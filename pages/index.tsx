ce8c1cb11affc2baae4f5a9ae2f8e91282a65cbc
adshao <tjusgj@gmail.com>
2023-03-23 23:37:55 +0800

    Fix compiler errors
    Signed-off-by: adshao <tjusgj@gmail.com>

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,7 @@
 import { Answer } from "@/components/Answer/Answer";
 import { Footer } from "@/components/Footer";
 import { Navbar } from "@/components/Navbar";
-import { PGChunk } from "@/types";
+import { Tweet } from "@/types";
 import { IconArrowRight, IconExternalLink, IconSearch } from "@tabler/icons-react";
 import endent from "endent";
 import Head from "next/head";
@@ -11,7 +11,7 @@ export default function Home() {
   const inputRef = useRef<HTMLInputElement>(null);
 
   const [query, setQuery] = useState<string>("");
-  const [chunks, setChunks] = useState<PGChunk[]>([]);
+  const [chunks, setChunks] = useState<Tweet[]>([]);
   const [answer, setAnswer] = useState<string>("");
   const [loading, setLoading] = useState<boolean>(false);
 
@@ -49,7 +49,7 @@ export default function Home() {
       throw new Error(searchResponse.statusText);
     }
 
-    const results: PGChunk[] = await searchResponse.json();
+    const results: Tweet[] = await searchResponse.json();
 
     setChunks(results);
 
@@ -89,7 +89,7 @@ export default function Home() {
       throw new Error(searchResponse.statusText);
     }
 
-    const results: PGChunk[] = await searchResponse.json();
+    const results: Tweet[] = await searchResponse.json();
 
     setChunks(results);
 
@@ -357,7 +357,7 @@ export default function Home() {
                       <div className="mt-4 border border-zinc-600 rounded-lg p-4">
                         <div className="flex justify-between">
                           <div>
-                            <div className="font-bold text-xl">{chunk.tweet_title}</div>
+                            {/* <div className="font-bold text-xl">{chunk.tweet_title}</div> */}
                             <div className="mt-1 font-bold text-sm">{chunk.tweet_date}</div>
                           </div>
                           <a
@@ -383,7 +383,7 @@ export default function Home() {
                     <div className="mt-4 border border-zinc-600 rounded-lg p-4">
                       <div className="flex justify-between">
                         <div>
-                          <div className="font-bold text-xl">{chunk.tweet_title}</div>
+                          {/* <div className="font-bold text-xl">{chunk.tweet_title}</div> */}
                           <div className="mt-1 font-bold text-sm">{chunk.tweet_date}</div>
                         </div>
                         <a
